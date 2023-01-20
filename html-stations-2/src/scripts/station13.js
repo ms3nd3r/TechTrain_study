da6306a50c5177dbc0df2b480d94f4d7f02639e7
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-01-20 09:49:06 +0900

    station13クリア

--- html-stations-2/src/scripts/station13.js
+++ html-stations-2/src/scripts/station13.js
@@ -1,4 +1,12 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const text = document.getElementById("text");
+  console.log(text);
+  const checkBox = document.getElementById("check");
+  console.log(checkBox);
+  if(checkBox.checked){
+    text.style.backgroundColor = "red";
+  }else{
+    text.style.backgroundColor = "transparent";
+  }
 }
