46bf48762675dd850820161ac51a32c5f58f19c2
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-01-24 20:59:27 +0900

    station3クリア

--- react-stations/src/App.js
+++ react-stations/src/App.js
@@ -11,6 +11,8 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <img src='https://images.dog.ceo/breeds/komondor/n02105505_3787.jpg'></img>
+      <h1>犬の画像を表示するサイトです</h1>
     </div>
   )
 }
