ce8226d0694ed322b098c9cbdca6a5aae804ee43
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-01-25 10:10:54 +0900

    station4クリア

--- react-stations/src/App.js
+++ react-stations/src/App.js
@@ -1,5 +1,5 @@
 // DO NOT DELETE
-
+import { useState } from 'react'
 import * as React from 'react'
 import './App.css'
 
@@ -7,11 +7,15 @@ import './App.css'
  * 
  * @type {React.FC}
  */
+
 export const App = () => {
+  const initialUrl = "https://images.dog.ceo/breeds/komondor/n02105505_3787.jpg"
+  const [dogUrl,setDogUrl] = useState(initialUrl)
   return (
     <div>
       <header>Dogアプリ</header>
-      <img src='https://images.dog.ceo/breeds/komondor/n02105505_3787.jpg'></img>
+      <img src={ dogUrl } width="300px"></img>
+      {/* <button onClick={ () => setDogUrl("https://images.dog.ceo/breeds/dalmatian/cooper2.jpg") }>チェンジ</button> */}
       <h1>犬の画像を表示するサイトです</h1>
     </div>
   )
