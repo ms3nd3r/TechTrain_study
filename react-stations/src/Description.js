7a9182390e84cac037a4911f501cb4af21bf45a1
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-02-27 15:26:06 +0900

    コンポーネント移行中

--- react-stations/src/Description.js
+++ react-stations/src/Description.js
@@ -1,28 +1,13 @@
 // DO NOT DELETE
 import React from "react";
-import { useEffect,useState } from 'react'
+import DogImage from "./DogImage";
 
 export default function Description(){
-    const initialUrl = "https://images.dog.ceo/breeds/komondor/n02105505_3787.jpg";
-    const [dogUrl,setDogUrl] = useState(initialUrl);
-  
-    function randomImage(){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) =>{
-          console.log(data.message);
-          setDogUrl(data.message);
-        })
-      }
-    
 
     return(
     <div>
-        <div className='main'>
-        <img src={ dogUrl }></img>
-        <button onClick={ () =>  randomImage() }>更新</button>
+      <div className='main'>
+        <DogImage dogUrl={ dogUrl } />
       </div>
       <h1>犬の画像を表示するサイトです</h1>
     </div>
--- react-stations/src/DogImage.js
+++ react-stations/src/DogImage.js
@@ -1,1 +1,29 @@
 // DO NOT DELETE
+import React from "react";
+import { useState } from 'react'
+
+export default function DogImage(props){
+
+    const initialUrl = "https://images.dog.ceo/breeds/komondor/n02105505_3787.jpg";
+    const [dogUrl,setDogUrl] = useState(initialUrl);
+  
+    function randomImage(){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) =>{
+          console.log(data.message);
+          setDogUrl(data.message);
+        })
+      }
+    
+
+    
+    return(
+        <div>
+            <img src={ props.dogUrl }></img>
+            <button onClick={ () =>  randomImage() }>更新</button>
+        </div>
+    );
+}
