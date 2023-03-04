e3d8c81872f279d95db8480cc7503b65b688d24b
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-03-04 14:42:43 +0900

    station09クリア

--- react-stations/src/DogListContainer.js
+++ react-stations/src/DogListContainer.js
@@ -4,21 +4,30 @@ import { useState,useEffect } from "react";
 
 export const DogListContainer = () => {
 
-    const initialBreeds = [];
-    const [breeds,setBreeds] = useState(initialBreeds); 
+    const [breeds,setBreeds] = useState([]); 
 
     useEffect(()=>{
         fetch("https://dog.ceo/api/breeds/list/all")
         .then((response) => {
           return response.json();
         })
+        .then(mes => {
+            const list = mes.message
+            setBreeds(Object.keys(list))
+
         // 全犬種を取得
         // messageの一層目を配列化
+        })
+
     },[])
 
     return(
         <div>
-            
+            <select name="breed" >
+                {breeds.map(breed => (
+                    <option key={breed} value={breed}>{breed}</option>
+                ))}
+            </select>
         </div>
     );
 }
