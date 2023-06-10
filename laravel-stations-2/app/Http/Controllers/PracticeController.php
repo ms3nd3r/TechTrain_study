f084a5eba3afb8d9a9f051047bea976ceb46a017
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2023-06-10 14:56:17 +0900

    laravel2 station04クリア

--- laravel-stations-2/app/Http/Controllers/PracticeController.php
+++ laravel-stations-2/app/Http/Controllers/PracticeController.php
@@ -2,22 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Practice;
+
 class PracticeController extends Controller
 {
     public function sample()
     {
-        return response('practice');
+        return view('practice');
     }
 
     public function sample2()
     {
         $test = 'practice2';
-        return response($test);
+            return view('practice2' ,['testParam' => $test]);
     }
 
     public function sample3()
     {
-        $test3 = 'test';
-        return response($test3);
+        $testParam = 'test';
+            return view('practice3' ,['testParam' => $testParam]); 
+    }
+
+    public function getPractice()
+    {
+        $practice = Practice::all();
+            return response()->json($practice);
     }
 }
